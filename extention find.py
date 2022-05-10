f1121c15f03010d633cda41590cfc7ddc783c20e
var-21 <105124810+var-21@users.noreply.github.com>
2022-05-10 20:01:07 +0530

    Add files via upload

---
+++ extention find.py
@@ -0,0 +1,9 @@
+n=input("Enter an extention file:")
+if ".py" in n:
+    print("the extention file is: PYTHON")
+if ".c" in n:
+    print("the extention file is: C")
+if ".java" in n:
+    print("the extention file is: JAVA")
+if ".c++" in n:
+    print("the extention file is: C++")
