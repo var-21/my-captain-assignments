9955a67791dbdefc25a0b5c41d9d699b679cd916
var-21 <105124810+var-21@users.noreply.github.com>
2022-05-15 19:43:07 +0530

    Update dict file.py

--- dict file.py
+++ dict file.py
@@ -1,11 +1,13 @@
-n = input("Enter a string:")
-a = {}
-for i in n:
-    if i not in a:
-        a[i] = 1
-    else:
-        a[i] = a[i]+1
+def most_frequent(n):
+    a = {}
+    for i in n:
+        if i not in a:
+            a[i] = 1
+        else:
+            a[i] = a[i]+1
+    decreasingOrderFrequency = dict(sorted(a.items(), key=lambda x: x[1], reverse=True))
+    for x, y in decreasingOrderFrequency.items():
+        print(x, "=", y, end=" ")
 
-decreasingOrderFrequency = dict(sorted(a.items(), key=lambda x: x[1], reverse=True))
-for x, y in decreasingOrderFrequency.items():
-    print(x, "=", y, end=" ")
+
+most_frequent(input("Enter a string:"))
