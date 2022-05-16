b123b64f5642d3b317ba46f0f15bf3a737318e36
var-21 <105124810+var-21@users.noreply.github.com>
2022-05-16 10:47:36 +0530

    Update extention find.py

--- extention find.py
+++ extention find.py
@@ -1,9 +1,4 @@
-n=input("Enter an extention file:")
-if ".py" in n:
-    print("the extention file is: PYTHON")
-if ".c" in n:
-    print("the extention file is: C")
-if ".java" in n:
-    print("the extention file is: JAVA")
-if ".c++" in n:
-    print("the extention file is: C++")
+filename = input("Input the Filename: ")
+f_extension = filename.split(".")
+if (f_extension[-1])=='py':
+    print("The extension of the file is : 'Python'")
